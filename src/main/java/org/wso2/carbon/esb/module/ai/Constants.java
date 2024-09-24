a204d3ed8d974df57a3e87206e04375524c9b8ac
Isuru Wijesiri <isurumaduranga.official@gmail.com>
2024-09-24 14:53:47 +0530

    Add output type support to chat mediator

---
+++ src/main/java/org/wso2/carbon/esb/module/ai/Constants.java
@@ -0,0 +1,6 @@
+package org.wso2.carbon.esb.module.ai;
+
+public class Constants {
+    public static final String CONNECTION_NAME = "name";
+    public static final String TENANT_INFO_DOMAIN = "tenant.info.domain";
+}
--- src/main/java/org/wso2/carbon/esb/module/ai/operations/ChatCompletion.java
+++ src/main/java/org/wso2/carbon/esb/module/ai/operations/ChatCompletion.java
@@ -22,8 +22,21 @@ import dev.langchain4j.service.AiServices;
 import org.apache.synapse.MessageContext;
 import org.wso2.carbon.esb.module.ai.AbstractAIMediator;
 
-interface Agent {
+
+interface StringAgent {
     String chat(String userMessage);
+}
+
+interface IntegerAgent {
+    Integer chat(String userMessage);
+}
+
+interface FloatAgent {
+    Float chat(String userMessage);
+}
+
+interface BooleanAgent {
+    Boolean chat(String userMessage);
 }
 
 /**
@@ -32,13 +45,12 @@ interface Agent {
  */
 public class ChatCompletion extends AbstractAIMediator {
 
-    String TEMPLATE_SYSTEM_PROMPT = "systemPrompt";
-    String TEMPLATE_PROMPT = "prompt";
-    String TEMPLATE_OUTPUT_NAME = "output";
-
-    String API_KEY = "ai_openai_apiKey";
-
-    String DEFAULT_SYSTEM_PROMPT = "You are a helpful assistant.";
+    private static final String TEMPLATE_SYSTEM_PROMPT = "systemPrompt";
+    private static final String TEMPLATE_PROMPT = "prompt";
+    private static final String TEMPLATE_OUTPUT_NAME = "output";
+    private static final String TEMPLATE_OUTPUT_TYPE = "outputType";
+    private static final String DEFAULT_SYSTEM_PROMPT = "You are a helpful assistant.";
+    private static final String API_KEY = "ai_openai_apiKey";
 
     @Override
     public void execute(MessageContext mc) {
@@ -47,23 +59,51 @@ public class ChatCompletion extends AbstractAIMediator {
         String systemPromptName = getMediatorParameter(mc, TEMPLATE_SYSTEM_PROMPT, String.class, false);
         String promptName = getMediatorParameter(mc, TEMPLATE_PROMPT, String.class, false);
         String output = getMediatorParameter(mc, TEMPLATE_OUTPUT_NAME, String.class, false);
+        String outputType = getMediatorParameter(mc, TEMPLATE_OUTPUT_TYPE, String.class, false);
 
-        // Load properties from message context
+        // Load properties from message context - Model configurations
         String apiKey = getProperty(mc, API_KEY, String.class, false);
         String systemPrompt = getProperty(mc, systemPromptName, String.class, false);
         String prompt = getProperty(mc, promptName, String.class, false);
 
         try {
-            OpenAiChatModel model = OpenAiChatModel.withApiKey(apiKey);
-            Agent agent = AiServices.builder(Agent.class)
-                    .chatLanguageModel(model)
-                    .systemMessageProvider(chatMemoryId -> systemPrompt != null ? systemPrompt : DEFAULT_SYSTEM_PROMPT)
+            OpenAiChatModel model = OpenAiChatModel.builder()
+                    .modelName("gpt-4o-mini")
+                    .apiKey(apiKey)
                     .build();
-            String answer = agent.chat(prompt);
-            mc.setProperty(output, answer);
+
+            Object answer = getChatResponse(outputType, model, systemPrompt, prompt);
+            if (answer != null) {
+                mc.setProperty(output, answer);
+            } else {
+                log.error("Invalid output type");
+                handleException("Invalid output type", mc);
+            }
         } catch (Exception e) {
-            log.error(e);
-	        handleException("Error while LLM chat completion", e, mc);
+            log.error("Error while LLM chat completion", e);
+            handleException("Error while LLM chat completion", e, mc);
         }
+    }
+
+    private Object getChatResponse(String outputType, OpenAiChatModel model, String systemPrompt, String prompt) {
+        switch (outputType.toLowerCase()) {
+            case "string":
+                return createAgent(StringAgent.class, model, systemPrompt).chat(prompt);
+            case "integer":
+                return createAgent(IntegerAgent.class, model, systemPrompt).chat(prompt);
+            case "float":
+                return createAgent(FloatAgent.class, model, systemPrompt).chat(prompt);
+            case "boolean":
+                return createAgent(BooleanAgent.class, model, systemPrompt).chat(prompt);
+            default:
+                return null;
+        }
+    }
+
+    private <T> T createAgent(Class<T> agentType, OpenAiChatModel model, String systemPrompt) {
+        return AiServices.builder(agentType)
+                .chatLanguageModel(model)
+                .systemMessageProvider(chatMemoryId -> systemPrompt != null ? systemPrompt : DEFAULT_SYSTEM_PROMPT)
+                .build();
     }
 }
