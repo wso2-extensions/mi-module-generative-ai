d8a386aa5b2b1d386e17ed751132bbb106a425fb
Isuru Wijesiri <isurumaduranga.official@gmail.com>
2024-09-26 23:09:27 +0530

    Add file system doc loader

---
+++ src/main/java/org/wso2/carbon/esb/module/ai/rag/DocumentLoader.java
@@ -0,0 +1,23 @@
+package org.wso2.carbon.esb.module.ai.rag;
+
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import dev.langchain4j.data.document.Document;
+import org.apache.synapse.MessageContext;
+import org.wso2.carbon.esb.module.ai.AbstractAIMediator;
+
+import java.util.List;
+
+
+public class DocumentLoader extends AbstractAIMediator {
+
+    @Override
+    public void execute(MessageContext mc) {
+        String documentsPath = getMediatorParameter(mc, "documentsPath", String.class, false);
+        String outputPropertyName = getMediatorParameter(mc, "outputProperty", String.class, false);
+
+        List<Document> documents = FileSystemDocumentLoader.loadDocumentsRecursively(documentsPath, new TextDocumentParser());
+
+        mc.setProperty(outputPropertyName, documents);
+    }
+}
